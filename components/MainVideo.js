37bc5202cb4a0daec4e34dfa80b099c3c9c92cda
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2022-06-30 14:57:23 +0100

    update

---
+++ components/MainVideo.js
@@ -0,0 +1,9 @@
+const MainVideo = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default MainVideo;
---
+++ components/VideoArea.js
@@ -0,0 +1,15 @@
+import MainVideo from '../components/MainVideo'
+
+
+const VideoArea = () => {
+    return (
+        <div>
+            <MainVideo />
+            <div>
+                    
+            </div>            
+        </div>
+    );
+}
+
+export default VideoArea;
